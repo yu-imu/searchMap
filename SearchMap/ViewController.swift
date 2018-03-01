15e1d4ed4d917aaee7fc988afb1cc36ef5812b88
imura <imura@synapse.am>
2018-03-01 19:32:43 +0900

    検索できる地図アプリを制作

--- SearchMap/ViewController.swift
+++ SearchMap/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        let searchKeyword = textField.text
+        let geocorder = CLGeocoder()
+        print(searchKeyword!)
+        geocorder.geocodeAddressString(searchKeyword!, completionHandler: { (placemarks:[CLPlacemark]?, error: Error?) in
+            
+            if let placemark = placemarks?[0] {
+                if let targetCoordinate = placemark.location?.coordinate {
+                    print(targetCoordinate)
+                    
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = targetCoordinate
+                    pin.title = searchKeyword
+                    
+                    self.displayMap.addAnnotation(pin)
+                    self.displayMap.region =
+                    MKCoordinateRegionMakeWithDistance(targetCoordinate, 500.0, 500.0)
+                }
+            }
+        })
+        return true
+    }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var displayMap: MKMapView!
+    
 }
 
