d31410f12a7cfb9629b7c2af1c694bc1a6b91f49
imura <imura@synapse.am>
2018-03-01 17:22:30 +0900

    Initial Commit

---
+++ SearchMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchMap
+//
+//  Created by imua yusei on 2018/03/01.
+//  Copyright © 2018年 imua yusei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
